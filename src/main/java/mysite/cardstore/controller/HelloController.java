ac278a1c941671ab1bbb877fe6e5116067148fc8
xxx30514 <xxx30514@gmail.com>
2022-10-05 17:20:30 +0800

    rest風格實現

--- src/main/java/mysite/cardstore/controller/HelloController.java
+++ src/main/java/mysite/cardstore/controller/HelloController.java
@@ -2,7 +2,7 @@ package mysite.cardstore.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +12,14 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String handle01() {
 		return "你好 Spring Boot initializer";
+	}
+	
+	@RequestMapping(value = "/user",method = RequestMethod.DELETE)
+	public String deleteUser() {
+		return "delete 測試";
+	}
+	@RequestMapping(value = "/user",method = RequestMethod.GET)
+	public String getUser() {
+		return "get 測試";
 	}
 }
