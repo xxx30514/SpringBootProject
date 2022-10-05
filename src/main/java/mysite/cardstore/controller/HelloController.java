50334ff737f6c349a1e574972a7fc07d28944ecd
xxx30514 <xxx30514@gmail.com>
2022-10-05 17:24:42 +0800

    rest 需要配置yam

--- src/main/java/mysite/cardstore/controller/HelloController.java
+++ src/main/java/mysite/cardstore/controller/HelloController.java
@@ -4,7 +4,17 @@ package mysite.cardstore.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+/**
+ * 
+ * @author yeh
+ *
+ *Rest風格實現 要在yam配置文件中開啟
+ *spring:
+  	mvc:
+     hiddenmethod:
+      filter:
+       enabled: true
+ */
 @RestController
 //@ResponseBody+@Controller
 public class HelloController {
@@ -13,7 +23,7 @@ public class HelloController {
 	public String handle01() {
 		return "你好 Spring Boot initializer";
 	}
-	
+	//spring security 有設定衝突 post方法會報錯403 尚待解決  
 	@RequestMapping(value = "/user",method = RequestMethod.DELETE)
 	public String deleteUser() {
 		return "delete 測試";
